68fe76171ceb8a831a746e1de800d6954c0b6e00
lisalogvinova <lisalogvinova@gmail.com>
2018-02-19 22:08:48 -0800

    palindrom int

--- app/src/main/java/com/example/borysgurtovyi/codinginterviewprep/PalindromIntK.kt
+++ app/src/main/java/com/example/borysgurtovyi/codinginterviewprep/PalindromIntK.kt
@@ -3,3 +3,18 @@ package com.example.borysgurtovyi.codinginterviewprep
 /**
  * Created by lisa on 18.02.2018.
  */
+class PalindromIntK{
+    fun isPalindrome(x: Int): Boolean {
+        //x = 123744598
+
+        var reversedX = 0
+        while (){
+            var reminder = x % 10
+            reversedX = reversedX + reminder
+            x = x / 10
+
+        }
+
+
+    }
+}
