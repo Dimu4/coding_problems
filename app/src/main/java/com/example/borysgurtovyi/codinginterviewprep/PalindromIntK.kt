0308629dd9622d7154b98bff5673797b268030af
lisalogvinova <lisalogvinova@gmail.com>
2018-02-19 20:33:04 -0800

    new file palindromintk added

---
+++ app/src/main/java/com/example/borysgurtovyi/codinginterviewprep/PalindromIntK.kt
@@ -0,0 +1,5 @@
+package com.example.borysgurtovyi.codinginterviewprep
+
+/**
+ * Created by lisa on 18.02.2018.
+ */
--- app/src/test/java/com/example/borysgurtovyi/codinginterviewprep/MoveZeroesTest.kt
+++ app/src/test/java/com/example/borysgurtovyi/codinginterviewprep/MoveZeroesTest.kt
@@ -15,3 +15,4 @@ class MoveZeroesTest {
         println(nums.asList())
     }
 }
+
